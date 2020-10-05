0d7b64c8a7b94e83b8be5c9d9b2eb6ca47f05064
Matthew Mccarthy <matthewmc96@gmail.com>
2020-10-04 20:50:57 -0500

    new commit

--- server/utils/router.js
+++ server/utils/router.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/join', (req, res) => {
   res.send({ response: 'Server is up and running.' }).status(200)
 })
 
